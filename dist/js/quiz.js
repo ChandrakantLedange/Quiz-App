8b74afad3c1f9a41ad7ab3427b39f210d28e5d67
ChandrakantLedange <chandrakant0039@gmail.com>
2021-05-06 23:47:11 +0530

    static questions build

--- dist/js/quiz.js
+++ dist/js/quiz.js
@@ -1,3 +1,40 @@
+//questions
+let questions = [
+  {
+    id: 1,
+    question: "What is the full form of RAM?",
+    answer: "Random Access Memory",
+    options: [
+      "Random Access Memory",
+      "Randomly Access Memory",
+      "Run Accept Memory",
+      "None of these",
+    ],
+  },
+  {
+    id: 2,
+    question: "What is the CPU?",
+    answer: "Central Processing Unit",
+    options: [
+      "Central Program Unit",
+      "Central Processing Unit",
+      "Central Preload Unit",
+      "None of these",
+    ],
+  },
+  {
+    id: 3,
+    question: "What is E-mail",
+    answer: "Electronic Mail",
+    options: [
+      "Electrocity Mail",
+      "Electro-magnet Mail",
+      "Electronic Mail",
+      "None of these",
+    ],
+  },
+];
+
 //submit form
 function submitForm(e) {
   e.preventDefault();
@@ -5,8 +42,14 @@ function submitForm(e) {
 
   //store user name
   sessionStorage.setItem("user_name", name);
-//   console.log(name);
+  //   console.log(name);
 
   //jump to quiz page
   location.href = "quiz.html";
+}
+
+//next question
+let question_count = 0;
+function next_question() {
+  console.log("works");
 }
